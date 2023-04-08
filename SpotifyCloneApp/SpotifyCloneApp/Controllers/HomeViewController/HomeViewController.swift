b95731cd7380ed7b7cf3cf33850fc399f31a9365
haze998 <docenko6996@gmail.com>
2023-04-08 15:42:06 +0300

    delete-storyboard-file-managed-folders

---
+++ SpotifyCloneApp/SpotifyCloneApp/Controllers/HomeViewController/HomeViewController.swift
@@ -0,0 +1,24 @@
+//
+//  HomeViewController.swift
+//  SpotifyCloneApp
+//
+//  Created by Evgeniy Docenko on 08.04.2023.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    // - MARK: Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+
+    private func setupUI() {
+        self.view.backgroundColor = .red
+    }
+    
+
+}
+
